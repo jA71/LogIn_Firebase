9cdee385ba34835b55db53929379ec12edfe6354
fernandoJoseC <fernandocalderon05@gmail.com>
2022-06-21 22:45:17 -0500

    cambios login

--- app/src/main/java/com/example/login_firebase/AuthActivity.kt
+++ app/src/main/java/com/example/login_firebase/AuthActivity.kt
@@ -1,11 +1,91 @@
 package com.example.login_firebase
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.login_firebase.databinding.ActivityAuthBinding
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var views: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        views = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        setup()
+        session()
+
     }
+
+    override fun onStart(){
+        super.onStart()
+        views.root.visibility = View.VISIBLE
+    }
+
+    private fun session() {
+
+        val prefs = getSharedPreferences(getString(com.example.login_firebase.R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        //si ya estamos logeados pasamos a la siguiente activity
+
+        if (email!=null && provider!=null){
+            views.root.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
+
+    }
+
+    private fun setup() {
+        views.loginBtn.setOnClickListener {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(views.user.text.toString(), views.password.text.toString()).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showHome(it.result?.user?.email ?: "",  ProviderType.BASIC)
+
+                }else{
+                    Toast.makeText(this, "Error, incorrect password", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        views.registerMsg.setOnClickListener {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(views.user.text.toString(), views.password.text.toString()).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showHome(it.result?.user?.email ?: "",  ProviderType.BASIC)
+
+                }else{
+                    Toast.makeText(this, "Error, user already exists", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        views.googleBtn.setOnClickListener {
+
+            //configuracion
+            //val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id))
+            FirebaseAuth.getInstance().
+
+        }
+
+
+    }
+
+    private fun showHome(email: String, provider: ProviderType){
+
+        val intent:Intent=Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(intent)
+    }
+
+
 }
---
+++ app/src/main/java/com/example/login_firebase/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.login_firebase
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.login_firebase.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE
+}
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var views: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        views = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(views.root)
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        //guardado de datos
+        val prefs =
+            getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+
+    }
+
+    private fun setup(email: String, provider: String) {
+        views.email.text = email
+        views.provider.text = provider
+        views.logOutBtn.setOnClickListener {
+
+            val prefs =
+                getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
+
+    }
+
+
+}
